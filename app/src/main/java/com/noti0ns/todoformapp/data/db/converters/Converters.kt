acd270d8234b160990b6fa1125645dd365d05b83
José Lucas da Silva <joselucas0303@gmail.com>
2023-08-22 22:42:55 -0300

    undo zoned datetime conversion

--- app/src/main/java/com/noti0ns/todoformapp/data/db/converters/Converters.kt
+++ app/src/main/java/com/noti0ns/todoformapp/data/db/converters/Converters.kt
@@ -1,13 +1,12 @@
 package com.noti0ns.todoformapp.data.db.converters
 
 import androidx.room.TypeConverter
-import java.time.Clock
 import java.time.LocalDateTime
 
 class Converters {
     @TypeConverter
     fun fromTimestamp(value: String?): LocalDateTime? = value?.let {
-        LocalDateTime.parse(value).atZone(Clock.systemDefaultZone().zone).toLocalDateTime()
+        LocalDateTime.parse(value)
     }
 
     @TypeConverter
